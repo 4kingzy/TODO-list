6a2b45258293a86fbc580a1313dbb1d3111b64f8
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2023-02-13 21:04:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const inputEl = document.querySelector(".todo__input");
+const errorMessageEl = document.querySelector(".todo__error-message");
 const todoTasksEl = document.querySelectorAll(".todo__task");
 const todoTasksList = document.querySelector(".todo__tasks");
+
 const tasks = [];
 inputEl.addEventListener("keydown", (event) => {
     if (event.key === "Enter"){
         event.preventDefault();
         const task = inputEl.value;
-        
-
-        tasks.push(task);
-
-        inputEl.value = "";
-        
-        renderToDoTasks(tasks);
+        if (task.length > 0)
+        if (tasks.indexOf(task) === -1) {
+            errorMessageEl.style.display = "none";
+            tasks.push(task);
+            inputEl.value = "";
+            renderToDoTasks(tasks);
+        } else {
+            errorMessageEl.style.display = "block";
+        }
+             
     }
 });
 
@@ -21,10 +26,22 @@ const renderToDoTasks = (tasks) => {
     tasks.forEach((task, index) => {
         const taskEl = document.createElement("div");
         taskEl.className = "todo__task";
-        taskEl.innerText = `${index + 1}. ${task}`;
+
+        const paragraphEl = document.createElement("p");
+        paragraphEl.innerText = `${index +1}. ${task}`;
+
+        const deleteEl = document.createElement("a");
+        deleteEl.className = "todo__task-delete";
+        deleteEl.innerText = "Delete";
+
+        taskEl.appendChild(paragraphEl);
+        taskEl.appendChild(deleteEl);
+
         todoTasksList.appendChild(taskEl);
     });
 }
+
+
 /* todoTasksEl.forEach((x) => x.addEventListener
     ("click", (event) => {
         console.log(event.target.innerText);
